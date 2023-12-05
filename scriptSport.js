2b7bd3c1208abe577808ca52d340c1878f65b12e
cedric chemin <cedric.chemin33@gmail.com>
2023-12-05 16:31:53 +0100

    mise en place menu deroulant

---
+++ scriptSport.js
@@ -0,0 +1,28 @@
+// Fonction pour basculer l'affichage du menu déroulant
+function toggleDropdown() {
+    const dropdown = document.getElementById("myDropdown");
+    dropdown.classList.toggle("show");
+}
+
+function toggleDropdown1() {
+    const dropdown = document.getElementById("myDropdown1");
+    dropdown.classList.toggle("show");
+}
+
+function toggleDropdown2() {
+    const dropdown = document.getElementById("myDropdown2");
+    dropdown.classList.toggle("show");
+}
+
+// Ferme le menu déroulant si l'utilisateur clique en dehors de celui-ci
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
